2c2b19f46b21652e9326259fb1bfbe0c95d17d11
enricoviolagit <enricoviola82@gmail.com>
2018-02-08 18:35:43 +0100

    Step 3

--- script.js
+++ script.js
@@ -19,4 +19,98 @@
     // Questo errore si avvia nel caso di Non supporto dei Serv Work da parte del browser
     alert('No service worker support in this browser');
   }
+
+
+  var statement = 'select * from weather.forecast where woeid=' + '2459115'; // NY
+  var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' + statement;
+  
+  var initialWeatherForecast = {
+      created: '2016-07-22T01:00:00Z',
+      channel: {
+        title: "Yahoo! Weather - New York, NY, US",
+        lastBuildDate: 'Mon, 05 Feb 2018 11:51 AM EST',
+        astronomy: {
+          sunrise: "5:43 am",
+          sunset: "8:21 pm"
+        },
+        item: {
+          condition: {
+            text: "Windy",
+            date: "Thu, 21 Jul 2016 09:00 PM EDT",
+            temp: 56,
+            code: 24
+          },
+          forecast: [
+            {code: 44, high: 86, low: 70},
+            {code: 44, high: 94, low: 73},
+            {code: 4, high: 95, low: 78},
+            {code: 24, high: 75, low: 89},
+            {code: 24, high: 89, low: 77},
+            {code: 44, high: 92, low: 79},
+            {code: 44, high: 89, low: 77}
+          ]
+        },
+        atmosphere: {
+          humidity: 56
+        },
+        wind: {
+          speed: 25,
+          direction: 195
+        }
+      }
+    };
+
+
+  if ('caches' in window) {
+    /*
+     * Check if the service worker has already cached this city's weather
+     * data. If the service worker has the data, then display the cached
+     * data while the app fetches the latest data.
+     */
+      caches.match(url).then(function(response) {
+          if (response) {
+              response.json().then(function updateFromCache(json) {
+                  var results = json.query.results;
+                  results.label = results.channel.title;
+                  results.created = json.query.created;
+                  console.log("Dentro Cache", results.label, results.created);
+                  updateForecastCard(results);
+              });
+          }
+      });
+  }
+
+
+  // Fetch the latest data.
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        var response = JSON.parse(request.response);
+        var results = response.query.results;
+        results.label = results.channel.title;
+        results.created = response.query.created;
+        updateForecastCard(results);
+      }
+    } else {
+      // Return the initial weather forecast since no data is available.
+      updateForecastCard(initialWeatherForecast);
+    }
+  };
+  request.open('GET', url);
+  request.send();
+
+  function updateForecastCard(data) {
+    var dataLastUpdated = new Date(data.created);    
+    var lastBuildDate = data.channel.lastBuildDate;
+    var title = data.channel.title;
+    var sunrise = data.channel.astronomy.sunrise;
+    var sunset = data.channel.astronomy.sunset;
+    var current = data.channel.item.condition;
+    var humidity = data.channel.atmosphere.humidity;
+    var wind = data.channel.wind;
+
+    console.log("Meteo", lastBuildDate, title, current, humidity, wind);
+  }
+
 })();
--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,6 @@ self.addEventListener('install', (event) => {
     // indichiamo un nome alla sezione di cache e dopo abbiamo una promise
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
 
         // con addAll aggiungiamo un array delle risorse che vogliamo inserire in cache, risorse considerate fondamentali per il funzionamento della App Shell
         return cache.addAll(urlsToCache);
@@ -25,76 +24,11 @@ self.addEventListener('install', (event) => {
 
 
 
-// Dopo l'installazione, i Service Workers risultano attivi. Andiamo ad aggiornare il service workers per ogni pagina differente o refresh
 
-// Un evento FETCH si attiva ogni volta che viene recuperata qualsiasi risorsa che potrebbe essere controllata dal Service Worker,
-// ciò i documenti all'interno dello Scope (scope di esecuzione del Serv Wor) specificato e qualsiasi risorsa a cui fa riferimento in tali documenti
-// (ad esempio se index.html effettua una richiesta per incorporare un'immagine, allora passa attraverso il suo operaio di servizio.)
+// STEP 3 -------- Iniziare da qui ----------------------------
+// AGGIORNAMENTO dati previsione meteo -----------------------------------------------------
 
-// Per prima cosa apriamo la cache e abbiniamo la richiesta con quelle presenti nella cache. Se corrispondono, restituiamo i dati dalla cache.
-// Se la richiesta non corrisponde, reindirizziamo la richiesta al server.
-// Quando i dati vengono ricevuti correttamente dal server, restituiamo tali dati.
-// Quindi apriamo la cache e salviamo i dati qui utilizzando cache.put () in modo che sia possibile accedervi direttamente dalla cache nei seguenti tentativi.
-
-
-self.addEventListener('fetch', function(event) {
-  console.log("refresh", event.request.url);
-
-  event.respondWith(
-
-    // "caches.match()" questo metodo esamina la richiesta e prova a cercare la stessa equivalente nella cache del service worker.
-    // La corrispondenza viene eseguita tramite url e vari header, proprio come con le normali richieste HTTP.
-    // Se trova una risposta corrispondente, restituisce quel valore dalla cache.
-    // Altrimenti restituisce il risultato della chiamata a "fetch" che effettuerà una richiesta alla rete, restituendone i dati.
-    
-    caches.match(event.request).then(function(response) {
-        // Cache trovata, ritorna la risorsa dalla cache
-        if (response) {
-          return response;
-        }
-
-        // Cache non trovata, la richiedo dalla rete
-        requestBackend(event);
-
-      })
-    );
-});
-
-function requestBackend(event){
-  console.log("requestBackend", event);
-
-  // Necessaria la clonazione, poiché tale richiesta può essere consumata 1 sola volta, qui ci serve 2 volte sia per richiederla alla cache sia alla rete
-  var url = event.request.clone();
-  return fetch(url).then(function(res){
-      // if not a valid response send the error
-      if(!res || res.status !== 200 || res.type !== 'basic'){
-          return res;
-      }
-
-      // Necessaria anche qui la clonazione per lo stesso motivo della richiesta
-      var response = res.clone();
-
-      caches.open(CACHE_NAME).then(function(cache){
-          cache.put(event.request, response);
-          console.log("requestBackend2", event.request, response);
-      });
-
-      return res;
-  })
-}
-
-
-// L'evento "activate" viene usato soprattutto per liberarsi della vecchia cache (oltre ad altro), per liberare spazio soprattutto tenendo conto
-// che ogni browser ha un limite alla quantità di memoria cache che ogni singolo service worker può usare (in caso il Browser gestirà direttamente
-// da sé l'ottimizzazione dello spazio)
-
-// In caso di aggiornamento della cache, ogni volta che i files vengono modificati:
-// 1) Aggiorniamo l'elenco dei dati da inserire in cache (CACHE_NAME) perché se il browser rileva eventuali modifiche nel Service Worker,
-// li scaricherà nuovamente. L'evento di installazione nel nuovo service worker verrà generato ma entrerà nella fase di 'attesa' in quanto
-// la pagina sarà ancora controllata dal vecchio Service Worker. 
-// 2) Quando tutte le istanze del tuo sito Web sono chiuse, il nuovo Service Worker prenderà il controllo al posto del precedente.
-// Si intende qui che nel caso di più finestre aperte nel nostro browser, finché Tutte non verranno ricaricate, il nuovo Service Worker rimarrà in attesa
-// 3) A questo punto verrà attivato l'evento di installazione e qui sarà necessario eseguire una gestione della cache.
+var dataCacheName = 'weatherData-v1';
 
 self.addEventListener('activate', function(event) {
 
@@ -102,7 +36,8 @@ self.addEventListener('activate', function(event) {
     caches.keys().then( function(cacheNames) {
       return Promise.all( cacheNames.map( function(cacheName, i) {
           
-          if(cacheName !== CACHE_NAME){
+        // Verifichiamo inoltre che non sia uguale all'oggetto Meteo già memorizzato
+          if(cacheName !== CACHE_NAME && key !== dataCacheName){
             return caches.delete(cacheName[i]);
           }
 
@@ -112,3 +47,63 @@ self.addEventListener('activate', function(event) {
   );
 });
 
+
+self.addEventListener('fetch', function(e) {
+
+    // Dove prelevare i dati del meteo
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+
+    // Se la richiesta URL fornisce dataUrl, la nostra app richiederà dati meteo aggiornati, quindi li recupera dalla rete e li salverà in cache.
+      console.log('0 [Service Worker] Fetch', e.request.url);
+    if (e.request.url.indexOf(dataUrl) > -1) {
+      console.log('1 [Service Worker] Fetch', e.request.url);
+
+      // Adatto per il Contenuto che si aggiorna frequentemente.
+      // L'idea è di mostrare prima i dati memorizzati nella cache, quindi aggiornare la pagina quando / se i dati di rete arrivano.
+      
+      /*
+      e.respondWith(
+        caches.open(dataCacheName).then(function(cache){
+        
+        // Necessaria la clonazione, poiché tale richiesta può essere consumata 1 sola volta, qui ci serve 2 volte sia per richiederla alla cache sia alla rete
+        var url = e.request.clone();
+        
+        return fetch(url).then(function(res){
+          // if not a valid response send the error
+          if(!res || res.status !== 200 || res.type !== 'basic'){
+              return res;
+        }
+
+        // Necessaria anche qui la clonazione per lo stesso motivo della richiesta
+        var response = res.clone();
+
+          cache.put(e.request.url, response);
+          return response;
+          console.log("requestBackend3", event.request, response);
+        });
+
+      })
+    );
+
+*/
+
+e.respondWith(
+  caches.open(dataCacheName).then(function(cache) {
+      return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+      });
+  })
+);
+  } else {
+    console.log('2 [Service Worker] Fetch', e.request.url);
+    
+    // Questo ti dà il comportamento "Solo cache" per le cose nella cache e il comportamento "Solo rete" per tutto ciò che non è memorizzato nella cache
+
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
